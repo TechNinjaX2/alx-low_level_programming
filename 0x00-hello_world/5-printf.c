7bb331994d00ac25d0695431e7dead3cdcac6e18
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-05 15:04:55 -0700

    5 take 3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Retturn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art \n");
+	printf("with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
