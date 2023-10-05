7e2ec36452d04bb12e62ea2a9d40a5b0ac4cbbed
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-05 15:01:40 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retturn: Always 0 (Success)
+ */
 int main()
 {
-	printf("with proper grammar, but the outcome s a piece of art \n");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art \n");
+	return (0);
 }
