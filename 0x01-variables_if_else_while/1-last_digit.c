93818db02bdf5a2ebf5deced60fa9dc1618b2c3d
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-08 13:07:26 -0700

    1 for  trial

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	z = ((n%10))
+	for (;n>5;)
+		printf("Last digit of %d", n "is %d",z "and is greater than 5")
+	return (0);
+}
