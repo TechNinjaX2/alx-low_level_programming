d7860c191cc62c677ec97e350d5c6069d58cf5c4
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-13 16:05:57 -0700

    3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,13 +2,19 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- *
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest
  */
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
 
 	if (a > b && a> c)
 	{
