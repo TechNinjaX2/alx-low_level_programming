3c21410d5a208c40b43f9b7cd454e4590ca6829d
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-15 11:23:51 -0700

    3 debug 1

---
+++ 0x03-debugging/3-main_a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - take a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
+ */
+
+int main(void)
