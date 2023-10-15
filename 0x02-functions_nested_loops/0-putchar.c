66c0f10982e20ca54741421b24959fe56156a379
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-15 14:09:37 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,14 @@
 int main(void)
 {
 	int c =_putchar;
-	putchar(c);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
