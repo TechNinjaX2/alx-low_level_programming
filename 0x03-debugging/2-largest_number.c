844722666e7edd0aa8068bdf906dc102cb7413a1
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-15 11:00:26 -0700

    2 debug 1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,13 +12,9 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	a = 972;
-	b = -98;
-	c = 0;
-
-	if (a > b && a> c)
+	if (a > b && a > c)
 	{
-		largest = a;		
+		largest = a;
 	}
 	else if (b > a && b > c)
 	{
