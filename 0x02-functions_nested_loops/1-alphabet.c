10bd272622aa0f5ef7f899624238a6386998b5e4
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-15 14:34:40 -0700

    1-alphabet 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints alphabets in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return(0);
+}
