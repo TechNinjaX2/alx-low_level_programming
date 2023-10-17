55e720591cb59aa41dfda294ac015b04ad4f051a
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-17 23:53:04 +0300

    5 pointer 0

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * rev_string - prints string in reverse
+ * @s: string to be printed
+ * Return: s
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i -= 0; i >= 0; i++)
+	{
+		putchar(s[i]);
+	}
+}
