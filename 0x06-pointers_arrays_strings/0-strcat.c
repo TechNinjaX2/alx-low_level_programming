d503b4c8f7433a3ce93910d7222f3b29ecc0b554
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-18 22:11:09 +0300

    0 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates string
+ * @dest: destination
+ * @src: source
+ * Return: str
+ */
+char *_strcat(char *dest, char *src)
+{
+	char dest[];
+	int i, j;
+	i = 0;
+	j = 0;
+
+	while ( s1[98] != '\0')
+	{
+		s1[i] = s1[j];
+		i++;
+		j++;
+	}
+	i = 0;
+	while ( s2[i] != '\0')
+	{
+		s1[i] = s2[j + i];
+		i++;
+	}
+	return;
+}
