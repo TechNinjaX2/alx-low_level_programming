4bccc36c7c3b05b18ac4fb54a48c6858635fc179
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-19 14:52:06 +0300

    0 day 2 0

--- 0x06-pointers_arrays_strings/0trial.c
+++ 0x06-pointers_arrays_strings/0trial.c
@@ -8,20 +8,37 @@
  * @src: source
  * Return: str
  */
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	i = 0;
 	j = 0;
+	src = strlen(s1);
 
-	while ( s1[98] != '\0')
-	{
-		i++;
-	}
 	while ( s2[j] != '\0')
 	{
-		 j++;
+		s1[src - 1 + j] = s2[j];
+		j++;
+		dest = s1[src - 1 + j];
 	}
-	putchar (s1[i] , s[j]);
+	putchar (dest);
 	return;
 }
