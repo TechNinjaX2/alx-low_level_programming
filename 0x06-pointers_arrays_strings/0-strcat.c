a1231a8e203a48e25bac4e9c993213c7d9573644
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-19 15:45:45 +0300

    0 day 1 3

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,22 @@
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+#include "main.h"
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
+/**
+ * _strcat - concatenates string
+ * @dest: destination
+ * @src: source
+ * Return: str
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	j = 0;
+	src = strlen(s1);
+
+	while ( s2[j] != '\0')
+	{
+		s1[src - 1 + j] = s2[j];
+		j++;
+		dest = s1[src - 1 + j];
+	}
+	putchar(dest);
 }
