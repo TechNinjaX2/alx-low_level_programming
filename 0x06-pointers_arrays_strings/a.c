6b1f5c600310e6a0b047efd8481e60af027b0229
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-19 15:32:55 +0300

    checker check

---
+++ 0x06-pointers_arrays_strings/a.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenates string
+ * @dest: destination
+ * @src: source
+ * Return: str
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	j = 0;
+	src = strlen(s1);
+
+	while ( s2[j] != '\0')
+	{
+		s1[src - 1 + j] = s2[j];
+		j++;
+		dest = s1[src - 1 + j];
+	}
+	putchar(dest);
+}
