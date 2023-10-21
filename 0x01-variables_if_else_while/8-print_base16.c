fd13bb29154814f2d839d8223e65068959b49105
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-21 16:47:42 +0300

    8 variables 0

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,13 +5,13 @@
  *
  * Return; Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int n, a;
-	
-	for(n = 0; n <= 9; n++)
+
+	for (n = '0'; n <= '9'; n++)
 		putchar(n);
-	for(a = 'a'; a <= 'f'; a++)
+	for (a = 'a'; a <= 'f'; a++)
 		putchar(a);
 	putchar('\n');
 	return (0);
