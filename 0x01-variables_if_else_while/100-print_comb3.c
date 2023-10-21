885f5912ca9311b4d3d8dfd926518203929543f5
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-21 17:01:39 +0300

    100 variables 0

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,19 @@ int main(void)
 {
 	int i, x;
 
-	for(i = '0'; i <= '8'; i++)
+	for (i = '0'; i <= '8'; i++)
 	{
-		for(x = i + 1; x <= '9'; x++)
+		for (x = i + 1; x <= '9'; x++)
 		{
-			if(x != i)
+			if (x != i)
 			{
 				putchar(i);
 				putchar(x);
-				putchar(', ');
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
