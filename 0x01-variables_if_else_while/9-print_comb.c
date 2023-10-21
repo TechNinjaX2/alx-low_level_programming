d6431abc50f1711357d017e7e4da068ae83021e7
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-21 16:52:43 +0300

    9 variables 0

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,13 @@
 int main(void)
 {
 	int i;
-	for(i = '0'; i <= '9';i++)
+	for(i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 		if(i != '9')
 		{
-			putchar(', ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
