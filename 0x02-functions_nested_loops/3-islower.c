98a5252db75bdc25d25c2416ccb507919909979c
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-22 21:00:22 +0300

    3 functions 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - check for lowcase alphabets
+ * @c: alphabets to be checked
+ * Return: 1 0r 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <=122)
+	{
+		return (1);
+	}
+	return (0);
+}
