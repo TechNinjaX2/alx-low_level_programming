7cde07cfa549866420a0877c8152ccd4aa6c6efa
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-22 23:15:49 +0300

    1 isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - checks for alphabets
+ * @c: characters
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c<= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
