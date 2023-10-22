05d03def760b6c42094ec2e7b23d648009d1557e
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-22 23:25:44 +0300

    sign 0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - checks for +ve & -ve numbers & 0
+ * @n: number to be cheked
+ * Return: + or - or 0
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		return (0-);
+	}
+	if (n > 0)
+	{
+		return (0+);
+	}
+	return (0);
+}
