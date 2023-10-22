7260c6c2d0da08486ea6d67a58442a4c3c8365a8
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-23 00:20:53 +0300

    sign 22

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,15 @@ int print_sign(int n)
 	{
 		putchar('-');
 	}
-	return (-1);
+	return ('-1');
 	if (n > '0')
 	{
 		putchar('+');
 	}
-	return (1);
+	return ('1');
 	if (n == '0')
 	{
 		putchar('0');
 	}
-	return (0);
+	return ('0');
 }
