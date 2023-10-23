77c2d4017e0375b0dbb6352c2c65b6a42f1f32b2
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-23 18:20:04 +0300

    memset 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - resets array memory
+ * @s: string
+ * @b: array element
+ * @n: array size
+ * return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	n = 95;
+	b = 0x01;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
