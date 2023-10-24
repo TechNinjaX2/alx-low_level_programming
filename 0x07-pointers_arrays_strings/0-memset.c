f3e04dfccc666afc6cfa3308536dc0c6dfda59ca
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-24 14:59:08 +0300

    day 1 0 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	if (i < n)
 	{
 		s[i] = b;
+		i++;
 	}
 	return (s);
 }
