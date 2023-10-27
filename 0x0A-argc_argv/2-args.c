9ed4cbad78dea53eeba515810f2dbf4753bae7dd
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-28 01:02:32 +0300

    1&2 argc 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,14 +5,14 @@
  * @argc: the number elements
  * @argv: the array
  * Return: Always 0(Success)
- */
+ */`
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 1; i != argc; i++)
 	{
-		printf("%s\n", *argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
