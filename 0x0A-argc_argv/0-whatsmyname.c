2ebb73ceabd23a798cc82b2fb99bc7a4219870f8
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-28 09:28:03 -0700

    day 1 args 0 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ * @argc: number of elments
+ * @argv: the array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	(void)argc;
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
