5618ea6a8420afc0a194e696f6476cd9f3bc62be
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-29 10:18:58 -0700

    day 1 3 args 0

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints result of multiplied arguments
+ * @argc: number of elements
+ * @argv: the array
+ * Return: Always 0(Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, a, b, r;
+
+	i = 1;
+	if (argc > 2)
+	{
+		a = *argv[i] - 48;
+		b = *argv[i + 1] - 48;
+		r = a * b;
+		printf("%d\n", r);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
