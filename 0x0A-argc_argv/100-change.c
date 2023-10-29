947c3f0273e53274f8e09200ad3fbd6088331e50
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-29 12:12:23 -0700

    100-change 2

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,39 +10,39 @@
  */
 int main(int argc, char *argv[])
 {
-	int r, s, t, u, v, w;
+	int r, z;
+	int s, s1, s2;
+        int t, t1, t2;
+	int u, u1, u2;
+	int v, v1, v2;
+        int w, w1, w2;
 
 	if (argc < 3)
 	{
 		r = atoi(argv[1]);
-		s = r / 25;
-		t = r / 10;
-		u = r / 5;
-		v = r / 2;
-		w = r / 1;
+		s = r % 25;
+		s1 = r - s;
+		s2 = s1 / 25;
+		t = s % 10;
+		t1 = s - t;
+		t2 = t1 / 10;
+		u = t % 5;
+		u1 = t - u;
+		u2 = u1 / 5;
+		v = u % 2;
+		v1 = u - v;
+		v2 = v1 / 2;
+		w = v % 1;
+		w1 = v - w;
+		w2 = w1 / 1;
+		z = s2 + t2 + u2 + v2 + w2;
 		if (r < 0)
 		{
 			printf("0\n");
 		}
-		if (s < t && s < u && s < v && s < w)
+		else
 		{
-			printf("%d\n", s);
-		}
-		if (t < s && t < u && t < v && t < w)
-		{
-			printf("%d\n", t);
-		}
-		if (u < s && u < t && u < v && u < w)
-		{
-			printf("%d\n", u);
-		}
-		if (v < s && v < t && v < u && v < w)
-		{
-			printf("%d\n", v);
-		}
-		if (w < s && w < t && w < u && w < v)
-		{
-			printf("%d\n", w);
+			printf("%d\n", z);
 		}
 	}
 	else
