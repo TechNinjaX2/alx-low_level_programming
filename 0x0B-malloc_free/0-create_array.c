a87bfab811fba8687ce9e565ce713922b624f97e
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-31 05:42:32 -0700

    day 2 create array 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	i = 0;
-	s = malloc(size * sizeof(char));
+	s = malloc(size * sizeof(*s));
 	if (size == 0)
 	{
 		return (NULL);
