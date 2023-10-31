8d0429ae3c94c9bb0fc1695f178524899e48d63b
Shadrack Muuo <phoenixnova427@gmail.com>
2023-10-31 14:39:19 -0700

    day 1 alloc_grid 0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates memory to a grid
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: s
+ */
+int **alloc_grid(int width, int height)
+{
+	int **s;
+	int k, l, i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	s = malloc(sizeof(int) * height);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		s[i] = malloc(sizeof(int) * width);
+		if (s[i] == NULL)
+		{
+			for (j = i; j >= 0; j--)
+			{
+				free(s[j]);
+			}
+			free(s);
+			return (NULL);
+		}
+	}
+	for (k = 0; k < height; k++)
+	{
+		for (l = 0; l < width; l++)
+		{
+			s[k][l] = 0;
+		}
+	}
+	return (s);
+}
