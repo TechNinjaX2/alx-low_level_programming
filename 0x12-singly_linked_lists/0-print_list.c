71820c929c4853d1ddc568bc43afd513f78f3429
shadrack <phoenixnova427@gmail.com>
2023-11-20 21:50:49 +0300

    0-prir_list.c is now betty compliant

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * element_number - counts the number of elements
+ * @h: struct for the elements
+ * @n: number of elements
+ * Return: n
+ */
 size_t element_number(const list_t *h, size_t n)
 {
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0](nil)\n");
+			printf("[0] (nil)\n");
 			return (element_number(h->next, n + 1));
 		}
-		printf("[%d]%s\n", h->len, h->str);
-		return element_number(h->next, n + 1);
+		printf("[%d] %s\n", h->len, h->str);
+		return (element_number(h->next, n + 1));
 	}
-	return n;
+	return (n);
 }
-
+/**
+ * print_list - prints the elements and returns the number
+ * @h: the struct
+ * Return: i(number of elements)
+ */
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 	size_t i = 0;
-        i = element_number(h, n);
-        return i;
+
+	i = element_number(h, n);
+	return (i);
 }
