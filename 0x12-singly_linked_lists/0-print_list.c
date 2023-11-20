c0cb6229e7cdf4153e145f40970d7e47f5b775a0
shadrack <phoenixnova427@gmail.com>
2023-11-20 21:03:33 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,42 +1,25 @@
 #include <stdio.h>
 #include "lists.h"
 
-/*int element_number(const list_t *h, int n)
-{
-	while (h != NULL)
-	{
-		printf("[%d]%s\n", h->len, h->str);
-		return element_number(h->next, n + 1);
-	}
-	return 0;
-}
-
-size_t print_list(const list_t *h)
-{
-	size_t n = 0;
-        element_number(h, n);
-        return n;
-}*/
-size_t print_list(const list_t *h)
+size_t element_number(const list_t *h, size_t n)
 {
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0](nil)\n");
-			return (print_list(h->next));
+			return (element_number(h->next, n + 1));
 		}
 		printf("[%d]%s\n", h->len, h->str);
-		return (print_list(h->next));
-	}
-	/*i = element_number(h, n);*/
-	return 2;
-}
-/*size_t element_number(const list_t *h, size_t n)
-{
-	while (h->str != NULL)
-	{
 		return element_number(h->next, n + 1);
 	}
 	return n;
-}*/
+}
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+	size_t i = 0;
+        i = element_number(h, n);
+        return i;
+}
