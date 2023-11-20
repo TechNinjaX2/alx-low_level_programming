197673888b318964d77b425ea49c76b1d70b78ec
shadrack <phoenixnova427@gmail.com>
2023-11-20 23:22:09 +0300

    2-add_node.c used strdup

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define print mr
+print;
+int main()
+{
+	printf("mgjjgj");
+	return 0;
+}
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,14 +3,19 @@
 #include <string.h>
 #include "lists.h"
 
+/**
+ * add_node - adds nodes to the list
+ * @head: a struct
+ * @str: string to be added
+ * Return: new_node
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = (list_t*)malloc(sizeof(char *));
-	char s = *str;
+	list_t *new_node = malloc(sizeof(char *));
 
-	new_node->str = s;
+	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = (*head);
 	(*head) = new_node;
-	return new_node;
+	return (new_node);
 }
