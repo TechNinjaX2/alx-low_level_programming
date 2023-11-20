97a14111afcd5e26748ba01f589ab093e0ea2f73
shadrack <phoenixnova427@gmail.com>
2023-11-20 22:18:24 +0300

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * list_len - counts number of elements
+ * @h: a struct
+ * Return: i(number of elements)
+ */
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
+	size_t i = 0;
 
-	while (h->next != NULL)
+	i = element_number(h, n);
+	return (i);
+}
+/**
+ * element_number - counts number of elements
+ * @h: a struct
+ * @n: number of elements
+ * Return: n
+ */
+size_t element_number(const list_t *h, size_t n)
+{
+	while (h != NULL)
 	{
-		n++;
-		h->next = h->next;
+		if (h->str == NULL)
+		{
+			return (element_number(h->next, n + 1));
+		}
+		return (element_number(h->next, n + 1));
 	}
-	return n;
+	return (n);
 }
