f55bed1617c36db1ba1f463f47a7de0b10552683
shadrack <phoenixnova427@gmail.com>
2025-03-31 16:50:36 +0300

    betty compliant

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,11 @@
 /**
  * main - prints a string
  *
- * Return : 1
- */ 
+ * Return : 1 on Success
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	write(2, "[Anything]", sizeof("[Anything]\n"));
 	return (1);
 }
